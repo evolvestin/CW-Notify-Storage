3a65147987fca3a50de42dc3ba5a512b8da4f109
steve10live <38049797+steve10live@users.noreply.github.com>
2019-01-24 16:07:48 +0300

    Create dim.py

---
+++ dim.py
@@ -0,0 +1,11 @@
+token = '530805773:AAFE-VDtmvxtCfJAzsTMTNBT74VWxDFguj4'
+file = 'new-eustorage'
+json_old = 'bigdata2.json'
+form = 'Lot #(\d+) : (.*)\n' \
+       '\S*\s?(.*)\n?' \
+       'Seller: (.*)\n' \
+       'Current price: (\d+) pouch\(es\)\n' \
+       'Buyer: (.+)\n' \
+       'End At: (\d\d) (.*) (.*) (\d\d):(\d\d)\n' \
+       'Status: (#active|Finished|Cancelled|Failed)'
+adress = 'https://t.me/ChatWarsAuction/'
