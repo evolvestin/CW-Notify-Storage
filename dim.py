4702e08c50e1130c1b5811dbf3045cf90f78b257
steve10live <38049797+steve10live@users.noreply.github.com>
2019-03-16 16:01:59 +0300

    Update dim.py

--- dim.py
+++ dim.py
@@ -1,23 +1,23 @@
-token = '468445195:AAHKJxXQRxg5kJau_KT4smoCvJfnqcgOS4c'
-server = 2
-file = 'new-storage'
-json_old = 'bigdata1.json'
-json_storage_supp = 'bigdata3.json'
-json_storage = 'bigdata4.json'
-json_active = 'bigdata5.json'
-form = 'Лот #(\d+) : (.*)\n' \
-    '\S*\s?(.*)\n?' \
-    'Продавец: (.*)\n' \
-    'Текущая .*: (\d+) 👝\n' \
-    'Покупатель: (.+)\n' \
-    'Срок: (\d\d) (.*) (.*) (\d\d):(\d\d)\n' \
-    'Статус: (#active|Finished|Cancelled|Failed)'
-adress = 'https://t.me/chatwars3/'
-lastsold = '\nПоследний проданный: '
-soldtimes = '\n\n<b>Продано раз: </b>'
-alltime = '<b>За всё время:</b>\n'
-days = '<b>За последние 7 дней:</b>\n'
-median = 'Медианная цена: '
-average = 'Средняя цена: '
-minmax = 'Мин/Макс: '
-unsold = 'Не продано: '
+token = '530805773:AAFE-VDtmvxtCfJAzsTMTNBT74VWxDFguj4'
+server = 1
+file = 'eustorage'
+json_old = 'eustorage1.json'
+json_storage_supp = 'eustorage3.json'
+json_storage = 'eustorage4.json'
+json_active = 'eustorage5.json'
+form = 'Lot #(\d+) : (.*)\n' \
+       '\S*\s?(.*)\n?' \
+       'Seller: (.*)\n' \
+       'Current price: (\d+) pouch\(es\)\n' \
+       'Buyer: (.+)\n' \
+       'End At: (\d\d) (.*) (.*) (\d\d):(\d\d)\n' \
+       'Status: (#active|Finished|Cancelled|Failed)'
+adress = 'https://t.me/ChatWarsAuction/'
+lastsold = '\nLast sold: '
+soldtimes = '\n\n<b>Times sold: </b>'
+alltime = '<b>All time:</b>\n'
+days = '<b>Last 7 days:</b>\n'
+median = 'Median: '
+average = 'Average: '
+minmax = 'Min/Max: '
+unsold = 'Unsold: '
