94d41b80a984886e93810f841fd9f687797fb5aa
steve10live <38049797+steve10live@users.noreply.github.com>
2019-12-03 18:16:22 +0300

    Update dim.py

--- dim.py
+++ dim.py
@@ -8,6 +8,7 @@ if cw == 'cw2':
     json_storage = 'eustorage4.json'
     json_active = 'eustorage5.json'
     adress = 'https://t.me/ChatWarsAuction/'
+    active = 'https://t.me/lot_updater/7?embed=1'
     # ---------------------- forms ----------------------
     title = 'Lot #(\d+) : (.*)'
     quali = 'Quality: (.*)'
@@ -36,6 +37,7 @@ else:
     json_storage = 'bigdata4.json'
     json_active = 'bigdata5.json'
     adress = 'https://t.me/chatwars3/'
+    active = 'https://t.me/lot_updater/8?embed=1'
     # ---------------------- forms ----------------------
     title = 'Лот #(\d+) : (.*)'
     quali = 'Качество: (.*)'
