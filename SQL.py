6258160ab2dd97f1045f67681e96bf94b8028865
steve10live <38049797+steve10live@users.noreply.github.com>
2020-05-25 22:24:57 +0300

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -12,23 +12,21 @@ class SQLighter:
         self.connection.close()
 
     # current_open
-    def create_lot(self, array):
+    def create_lot(self, values):
         with self.connection:
             self.cursor.execute('INSERT INTO old (au_id, lot_id, enchant, item_name, quality, '
                                 'condition, modifiers, seller, cost, buyer, stamp, status) '
-                                'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (*array,))
-
-    def create_new_lot(self, au_id):
-        with self.connection:
-            self.cursor.execute('INSERT INTO actives (au_id) VALUES (?)', (au_id,))
+                                'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (*values,))
 
     def create_lots(self, sql):
         with self.connection:
-            self.cursor.execute(sql)
+            result = self.cursor.execute(sql)
+            print('блять', result)
 
-    def delete_new_lot(self, au_id):
+    def update_lot(self, lot):
         with self.connection:
-            self.cursor.execute('DELETE FROM actives WHERE au_id = ?', (au_id,))
+            self.cursor.execute('UPDATE old SET cost=?, buyer=?, stamp=?, status=? WHERE au_id = ?',
+                                (lot['cost'], lot['buyer'], lot['stamp'], lot['status'], lot['au_id'],))
 
     def get_double(self):
         with self.connection:
@@ -39,40 +37,36 @@ class SQLighter:
             else:
                 return False
 
-    def get_lots(self, name):
+    def get_lots(self, item_name):
         with self.connection:
-            result = self.cursor.execute('SELECT * FROM old WHERE name = ? ORDER BY stamp', (name,)).fetchall()
-        if result:
-            return result
-        else:
-            return False
+            result = self.cursor.execute('SELECT * FROM old WHERE item_name = ? '
+                                         'AND NOT status = ? ORDER BY stamp', (item_name, '#active',)).fetchall()
+            result_array = []
+            for i in result:
+                result_array.append(i)
+            return result_array
+
 
     def get_quality(self, name):
         with self.connection:
-            result = self.cursor.execute('SELECT DISTINCT quality FROM old WHERE name = ?', (name,)).fetchall()
-        if result:
-            return result
-        else:
-            return False
-
-    def get_new_au_id(self):
-        with self.connection:
-            result = self.cursor.execute('SELECT au_id FROM actives').fetchall()
-        if result:
+            result = self.cursor.execute('SELECT DISTINCT quality FROM old WHERE item_name = ?', (name,)).fetchall()
             result_array = []
             for i in result:
                 result_array.append(i[0])
             return result_array
-        else:
-            return []
+
+    def get_active_au_id(self):
+        with self.connection:
+            result = self.cursor.execute('SELECT au_id FROM old WHERE status = ?', ('#active',)).fetchall()
+            result_array = []
+            for i in result:
+                result_array.append(i[0])
+            return result_array
 
     def get_au_id(self):
         with self.connection:
             result = self.cursor.execute('SELECT au_id FROM old').fetchall()
-        if result:
             result_array = []
             for i in result:
                 result_array.append(i[0])
             return result_array
-        else:
-            return []
